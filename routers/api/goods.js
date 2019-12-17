561394296655d3631df0ead343d083893ea1f3d2
柳泽溪 <1300922025@qq.com>
2019-12-17 15:17:50 +0800

    完成查询单个商品,修改商品,分类查询商品接口

--- routers/api/goods.js
+++ routers/api/goods.js
@@ -5,7 +5,7 @@ const { Type } = require('../../models/Type')
 const passport = require('passport')
 
 // hasMany
-// Type.hasMany(Goods, { foreignKey: 'type_id', as: 'goods' })
+Type.hasMany(Goods, { foreignKey: 'type_id', as: 'goods' })
 //belongsTo
 Goods.belongsTo(Type, { foreignKey: 'type_id', as: 'goodstype' })
 
@@ -58,6 +58,7 @@ router.post('/addgoods', passport.authenticate("jwt", { session: false }), (req,
         return res.status(200).json({ status: 0, msg: '添加失败,您不是管路员' })
     }
 })
+
 /**
  * $router POST /api/goods/deletegoodsbyid
  * @desc 通过id上架或下架商品
@@ -83,4 +84,58 @@ router.post('/deletegoodsbyid', passport.authenticate("jwt", { session: false })
     }
 })
 
+/**
+ * $router GET /api/goods/getgoodsbyid
+ * @desc 通过id查询商品
+ * @desc return goods
+ * @access  private
+ * @prams id:string|number
+ */
+router.get('/getgoodsbyid', (req, res) => {
+    let id = req.query.id || 0
+    Goods.findOne({
+        where: { id: id }
+    }).then(data => {
+        return res.status(200).json({ status: 1, msg: '查询成功', data: data })
+    })
+})
+
+/**
+ * $router POST /api/goods/updategoods
+ * @desc 通过id更新商品
+ * @desc return goods
+ * @access  private
+ * @prams id:string|number || goods
+ */
+router.post('/updategoods', passport.authenticate("jwt", { session: false }), (req, res) => {
+    let id = req.body.id || 0
+    let goods = JSON.parse(req.body.goods) || {}
+    Goods.update(goods, { where: { id: id } }).then(data => {
+        res.status(200).json({ status: 1, msg: '修改成功', data })
+    })
+})
+
+/**
+ * $router POST /api/goods/getgoodsbytype
+ * @desc 通过type_id查询商品
+ * @desc return goods[]
+ * @access  private
+ * @prams typeid:number|string
+ */
+router.get('/getgoodsbytype', (req, res) => {
+    let typeid = req.query.id || 0
+    let page = Number(req.query.page) || 1
+    let pageSize = Number(req.query.pageSize) || 10
+    Type.findAndCountAll({
+        where: { id: typeid },
+        include: {
+            model: Goods,
+            as: 'goods'
+        },
+        offset: (page - 1) * pageSize,//开始的数据索引，比如当page=2 时offset=10 ，而pagesize我们定义为10，则现在为索引为10，也就是从第11条开始返回数据条目
+        limit: pageSize//每页限制返回的数据条数
+    }).then(data => {
+        res.status(200).json({ status: 1, msg: '查询成功', data })
+    })
+})
 module.exports = router
