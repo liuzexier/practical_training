16e80f64cf12147a6d65ce8fc063195bbb3b950c
柳泽溪 <1300922025@qq.com>
2019-12-17 12:25:25 +0800

    增加下架商品,和上架商品

--- routers/api/goods.js
+++ routers/api/goods.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { Goods, Op } = require('../../models/Goods')
 const { Type } = require('../../models/Type')
+const passport = require('passport')
 
 // hasMany
 // Type.hasMany(Goods, { foreignKey: 'type_id', as: 'goods' })
@@ -32,24 +33,54 @@ router.get('/findbypage', (req, res) => {
 
 /**
  * $router GET /api/goods/addgoods
+ * @desc 添加商品
  * @desc return goods
  * @access  private
  */
-router.post('/addgoods', (req, res) => {
-    Goods.create({
-        title: req.body.title || '未命名',
-        create_date: (new Date).valueOf(),
-        type_id: req.body.type_id || 1,
-        price: req.body.price || 1,
-        description: req.body.description || '',
-        count: req.body.count || 1,
-        status: req.body.status || 1
-    }).then(data => {
-        // res.send(data)
-        return res.status(200).json({ status: 1, msg: '添加成功', data: data })
-    }).catch(err => {
-        return res.status(500).json({ status: 0, msg: '添加失败', err: err })
-    })
+router.post('/addgoods', passport.authenticate("jwt", { session: false }), (req, res) => {
+    // console.log(req.user)
+    if (req.user.identity === 'admin') {
+        Goods.create({
+            title: req.body.title || '未命名',
+            create_date: (new Date).valueOf(),
+            type_id: req.body.type_id || 1,
+            price: req.body.price || 1,
+            description: req.body.description || '',
+            count: req.body.count || 1,
+            status: req.body.status || 1
+        }).then(data => {
+            // res.send(data)
+            return res.status(200).json({ status: 1, msg: '添加成功', data: data })
+        }).catch(err => {
+            return res.status(500).json({ status: 0, msg: '添加失败', err: err })
+        })
+    } else {
+        return res.status(200).json({ status: 0, msg: '添加失败,您不是管路员' })
+    }
+})
+/**
+ * $router POST /api/goods/deletegoodsbyid
+ * @desc 通过id上架或下架商品
+ * @desc return goods
+ * @access  private
+ * @prams ids:Array<string|number> || status:number|string
+ */
+router.post('/deletegoodsbyid', passport.authenticate("jwt", { session: false }), (req, res) => {
+    if (req.user.identity === 'admin') {
+        let ids = JSON.parse(req.body.ids).map(item => {
+            let itemObj = {}
+            itemObj.id = item
+            req.body.status == 0 ? itemObj.status = '0' : itemObj.status = '1'
+            return itemObj
+        })
+        Goods.bulkCreate(ids, { updateOnDuplicate: ['status'] }).then(data => {
+            return res.status(200).json({ status: 1, msg: req.body.status == 0 ? '下架成功' : '上架成功', data })
+        }).catch(err => {
+            return res.status(500).json(err)
+        });
+    } else {
+        return res.status(200).json({ status: 0, msg: '添加失败,您不是管路员' })
+    }
 })
 
 module.exports = router
